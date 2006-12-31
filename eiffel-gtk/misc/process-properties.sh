622bf32b2d03c1d8c616d09da715cfe87c519535
tybor <tybor@f7d703df-6b0f-0410-9a7c-ef81f5045915>
2006-12-31 09:09:09 +0000

    Adding miscellaneous directory, with a couple of script to process GTK+ documentation and to extract properties name, type and access rules.
    git-svn-id: http://svn.gna.org/svn/eiffel-libraries/trunk@467 f7d703df-6b0f-0410-9a7c-ef81f5045915

---
+++ eiffel-gtk/misc/process-properties.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+(for doc in /usr/share/doc/libgtk2.0-doc/gtk/Gtk*.html; do
+    # class=$(basename $doc)
+    # class=${class%.html}
+    links -dump -no-numbering $doc | \
+	egrep "\"[a-z-]+\"[[:space:]]*[A-Za-z]+[[:space:]]*:.*"
+    done
+) | uniq | sort
