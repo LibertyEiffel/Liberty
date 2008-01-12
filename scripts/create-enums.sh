78bc8a5c31d66484987b1765c4dff72402f1ec66
tybor <tybor@f7d703df-6b0f-0410-9a7c-ef81f5045915>
2008-01-12 09:17:33 +0000

    WIP on create enum script
    git-svn-id: http://svn.gna.org/svn/eiffel-libraries/trunk@963 f7d703df-6b0f-0410-9a7c-ef81f5045915

--- scripts/create-enums.sh
+++ scripts/create-enums.sh
@@ -1,22 +1,52 @@
 #!/bin/sh
 
-process () {
-    FILE=$1
-    echo Processing $FILE
-    #STARTING_POINTS=$(grep -b "\benum\b" $FILE| cut -d ":" -f 1 | head -1 )
-    #ENDING_POINTS=$(grep -b "}.*;" $FILE | cut -d ":" -f 1 |head -1 )
-    #echo Starting point: $STARTING_POINTS
-    #echo Ending points: $ENDING_POINTS
-    PREFIX=$(mktemp)
-
-    csplit $FILE --prefix=$PREFIX "/\benum\b/" "/}/1"
-
-    
-    #if [ -n "$ENUM_BODY" ] ; then	echo "=== $ENUM_BODY ==="    fi
+eiffellize () {
+    ## Eiffellize class name 
+    if [ -n $1 ] ; then 
+	echo $1 | 
+	sed 's/\B\([A-Z]\)/_\1/g' |
+	tr a-z- A-Z_
+    fi
+}
+process_file () {
+    if [ -n $1 ] ; then 
+	FILE=$1
+	echo Processing $FILE
+        
+        # Loop initialization
+	PROCESSABLE=$(mktemp -t create-eiffel-enum-processable-XXXX)
+	PROCESSED=$(mktemp -t partially-processed-enum-XXXX)
+	# echo Processable $PROCESSABLE Processed $PROCESSED
+	cp $FILE $PROCESSABLE 
+	
+	while csplit --quiet --prefix=$PROCESSED $PROCESSABLE "/\benum\b/" "/}/1" 2>/dev/null
+	do
+	    cp ${PROCESSED}02 $PROCESSABLE
+	    ENUM=${PROCESSED}01
+	    process_enum $ENUM
+	done
+	rm $PROCESSABLE ${PROCESSED} ${PROCESSED}00 ${PROCESSED}01 ${PROCESSED}02 2>/dev/null
+    else
+	echo process_file got no file to work on
+    fi
 }
 
+process_enum () {
+    if [ -n $1 ] ; then 
+	ENUM_FILE=$1
+	ENUM_NAME=$(grep ";" $ENUM_FILE | tr --delete --complement [:alnum:] )
+	EIFFEL_ENUM=$(eiffellize $ENUM_NAME)
+	echo Enumeration $ENUM_NAME into $EIFFEL_ENUM
+	ENUM_PART=$(mktemp)
+	csplit --quiet --prefix=$ENUM_PART $ENUM_FILE "/{/1" "/}/" 
+	echo Enumeration items:
+	cat ${ENUM_PART}01 | tr --delete ","
+	echo =======
+	rm ${ENUM_PART}* 2>/dev/null
+    else echo No enum file to work on
+    fi
+}
 
-for FILE in $@ 
-do
-    process $FILE
+for FILE in $@ ; do
+    process_file $FILE
 done
