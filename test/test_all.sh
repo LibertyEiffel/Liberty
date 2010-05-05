e801cfd9c50bb75451c26d1ae63e8bbe3edbcc94
Cyril Adrian <cyril.adrian@gmail.com>
2010-05-05 19:08:54 +0200

    Still working on tests infrastructure.

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)
-export LIBERTY_TEST_HOME=$(pwd)
-cd ..
+ROOT=$(pwd)
+cd $(dirname $0)/..
 export LIBERTY_HOME=$(pwd)
-export LIBERTY_INTERPRETER=$(find $LIBERTY_HOME/src/tools/main -name libertyi_boost -type f)
+export LIBERTY_INTERPRETER=${LIBERTY_INTERPRETER:-$(find $LIBERTY_HOME/src/tools/main -name libertyi_boost -type f)}
 
-exec find $TEST_HOME -name test_\*.e -exec $LIBERTY_HOME/work/do_test.sh {} \;
+exec find $ROOT -name test_\*.e -exec $LIBERTY_HOME/work/do_test.sh {} \;
--- work/do_test.sh
+++ work/do_test.sh
@@ -3,10 +3,14 @@
 test=$(basename $1)
 cd $(dirname $1)
 
-test -e loadpath.se || {
-	echo './'
+{
+	test -e loadpath.se && cat loadpath.se | grep -v '^'"$LIBERTY_HOME/src/loadpath.se"
+	test -e loadpath.se || echo './'
 	echo "$LIBERTY_HOME/src/loadpath.se"
-} > loadpath.se
+} > loadpath.se~
+mv loadpath.se~ loadpath.se
 
-echo "Executing ${test%.e} in $(pwd)"
-exec $LIBERTY_INTERPRETER ./loadpath.se $(echo ${test%.e} | tr '[a-z]' '[A-Z]') make -vsys=$LIBERTY_HOME/work/ -log=trace -check=all
+eval `se -environment | grep -v '^#'`
+
+echo $LIBERTY_INTERPRETER ./loadpath.se $(echo ${test%.e} | tr '[a-z]' '[A-Z]') make -vsys=$LIBERTY_HOME/work/ -vpath_tutorial=${path_lib%/lib/}/tutorial/ -log=error -check=all
+exec $LIBERTY_INTERPRETER ./loadpath.se $(echo ${test%.e} | tr '[a-z]' '[A-Z]') make -vsys=$LIBERTY_HOME/work/ -vpath_tutorial=${path_lib%/lib/}/tutorial/ -log=error -check=all
