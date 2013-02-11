688cf82967328b49c455ccfd18d4ae61867edee7
tybor <paolo.redaelli@gmail.com>
2013-02-11 16:06:34 +0100

    FFI low-level generated wrappers

---
+++ src/wrappers/ffi/library/externals/plugin/c/plugin.h
@@ -0,0 +1,80 @@
+/*
+** This file have been created by wrapper-generator.
+** Any change will be lost by the next execution of the tool.
+*/
+#define ffi_type_struct_get_size(a_structure) (((struct _ffi_type*) (a_structure))->size)
+
+#define ffi_type_struct_set_size(a_structure,a_value) do {(((struct _ffi_type*)(a_structure)))->size = (a_value);}while(0)
+
+#define ffi_type_struct_get_alignment(a_structure) (((struct _ffi_type*) (a_structure))->alignment)
+
+#define ffi_type_struct_set_alignment(a_structure,a_value) do {(((struct _ffi_type*)(a_structure)))->alignment = (a_value);}while(0)
+
+#define ffi_type_struct_get_type(a_structure) (((struct _ffi_type*) (a_structure))->type)
+
+#define ffi_type_struct_set_type(a_structure,a_value) do {(((struct _ffi_type*)(a_structure)))->type = (a_value);}while(0)
+
+#define ffi_type_struct_get_elements(a_structure) (((struct _ffi_type*) (a_structure))->elements)
+
+#define ffi_type_struct_set_elements(a_structure,a_value) do {(((struct _ffi_type*)(a_structure)))->elements = (a_value);}while(0)
+
+#define sizeof__ffi_type (sizeof(struct _ffi_type))
+#define ffi_cif_struct_get_abi(a_structure) ((( ffi_cif*) (a_structure))->abi)
+
+#define ffi_cif_struct_set_abi(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->abi = (a_value);}while(0)
+
+#define ffi_cif_struct_get_nargs(a_structure) ((( ffi_cif*) (a_structure))->nargs)
+
+#define ffi_cif_struct_set_nargs(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->nargs = (a_value);}while(0)
+
+#define ffi_cif_struct_get_arg_types(a_structure) ((( ffi_cif*) (a_structure))->arg_types)
+
+#define ffi_cif_struct_set_arg_types(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->arg_types = (a_value);}while(0)
+
+#define ffi_cif_struct_get_rtype(a_structure) ((( ffi_cif*) (a_structure))->rtype)
+
+#define ffi_cif_struct_set_rtype(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->rtype = (a_value);}while(0)
+
+#define ffi_cif_struct_get_bytes(a_structure) ((( ffi_cif*) (a_structure))->bytes)
+
+#define ffi_cif_struct_set_bytes(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->bytes = (a_value);}while(0)
+
+#define ffi_cif_struct_get_flags(a_structure) ((( ffi_cif*) (a_structure))->flags)
+
+#define ffi_cif_struct_set_flags(a_structure,a_value) do {((( ffi_cif*)(a_structure)))->flags = (a_value);}while(0)
+
+#define sizeof_ffi_cif (sizeof( ffi_cif))
+#define ffi_raw_closure_struct_get_tramp(a_structure) ((( ffi_raw_closure*) (a_structure))->tramp)
+
+#define ffi_raw_closure_struct_set_tramp(a_structure,a_value) do {((( ffi_raw_closure*)(a_structure)))->tramp = (a_value);}while(0)
+
+#define ffi_raw_closure_struct_get_cif(a_structure) ((( ffi_raw_closure*) (a_structure))->cif)
+
+#define ffi_raw_closure_struct_set_cif(a_structure,a_value) do {((( ffi_raw_closure*)(a_structure)))->cif = (a_value);}while(0)
+
+#define ffi_raw_closure_struct_get_fun(a_structure) ((( ffi_raw_closure*) (a_structure))->fun)
+
+#define ffi_raw_closure_struct_set_fun(a_structure,a_value) do {((( ffi_raw_closure*)(a_structure)))->fun = (a_value);}while(0)
+
+#define ffi_raw_closure_struct_get_user_data(a_structure) ((( ffi_raw_closure*) (a_structure))->user_data)
+
+#define ffi_raw_closure_struct_set_user_data(a_structure,a_value) do {((( ffi_raw_closure*)(a_structure)))->user_data = (a_value);}while(0)
+
+#define sizeof_ffi_raw_closure (sizeof( ffi_raw_closure))
+#define ffi_java_raw_closure_struct_get_tramp(a_structure) ((( ffi_java_raw_closure*) (a_structure))->tramp)
+
+#define ffi_java_raw_closure_struct_set_tramp(a_structure,a_value) do {((( ffi_java_raw_closure*)(a_structure)))->tramp = (a_value);}while(0)
+
+#define ffi_java_raw_closure_struct_get_cif(a_structure) ((( ffi_java_raw_closure*) (a_structure))->cif)
+
+#define ffi_java_raw_closure_struct_set_cif(a_structure,a_value) do {((( ffi_java_raw_closure*)(a_structure)))->cif = (a_value);}while(0)
+
+#define ffi_java_raw_closure_struct_get_fun(a_structure) ((( ffi_java_raw_closure*) (a_structure))->fun)
+
+#define ffi_java_raw_closure_struct_set_fun(a_structure,a_value) do {((( ffi_java_raw_closure*)(a_structure)))->fun = (a_value);}while(0)
+
+#define ffi_java_raw_closure_struct_get_user_data(a_structure) ((( ffi_java_raw_closure*) (a_structure))->user_data)
+
+#define ffi_java_raw_closure_struct_set_user_data(a_structure,a_value) do {((( ffi_java_raw_closure*)(a_structure)))->user_data = (a_value);}while(0)
+
+#define sizeof_ffi_java_raw_closure (sizeof( ffi_java_raw_closure))
