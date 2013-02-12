659a2708d3fb8795a8dd1011acfc1e4199c9c8d7
tybor <paolo.redaelli@gmail.com>
2013-02-12 01:49:37 +0100

    cairo: adding generated low-level wrappers

---
+++ src/wrappers/cairo/library/externals/plugin/c/plugin.c
@@ -0,0 +1,7 @@
+/*
+** This file have been created by wrapper-generator.
+** Any change will be lost by the next execution of the tool.
+*/
+#ifndef CAIRO_LIBERTY_PLUGIN
+#	define CAIRO_LIBERTY_PLUGIN
+#endif
