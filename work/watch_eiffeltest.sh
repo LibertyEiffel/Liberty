89468969c5b47ed2557494b901e831bada36ec59
Cyril Adrian <cyril.adrian@gmail.com>
2013-06-06 08:28:03 +0200

    colors make more sense this way.

--- work/watch_eiffeltest.sh
+++ work/watch_eiffeltest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec watch -d --color -n 1 'find . -name eiffeltest -exec test -e {}/log.ref \; -exec test -e {}/log.new \; -exec diff -u {}/log.ref {}/log.new \; | diffstat -C -w 120 | sed "s!/eiffeltest/log.new!!;s!eiffeltest/log.new  !.!"'
+exec watch -d --color -n 1 'find . -name eiffeltest -exec test -e {}/log.ref \; -exec test -e {}/log.new \; -exec diff -u {}/log.new {}/log.ref \; | diffstat -C -w 120 | sed "s!/eiffeltest/log.ref!!;s!eiffeltest/log.ref  !.!"'
