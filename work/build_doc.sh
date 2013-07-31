8ccfed38ee8497d639f10937ccb6dd9766f2f062
Cyril Adrian <cyril.adrian@gmail.com>
2013-07-31 16:19:13 +0200

    doc zip WIP #12741

--- work/build_doc.sh
+++ work/build_doc.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
 export plain=${plain:-FALSE}
-if [ x$1 = x-plain ]; then
-    plain=TRUE
-fi
+export zip=${zip:-FALSE}
+
+while [ -n "$1" ]; do
+    case $1 in
+        -plain)
+            plain=TRUE
+            ;;
+        -zip)
+            zip=TRUE
+            ;;
+        *)
+            echo "Unknown argument: $1" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 root=$(cd $(dirname $(readlink -f $0))/..; pwd)
 DOC_ROOT=${DOC_ROOT:-$root/website/doc}
 export DOC_ROOT
 export LOG=${LOG:-$DOC_ROOT/build_doc$(date +'-%Y%m%d-%H%M%S').log}
+
+echo "Log is $LOG"
 
 . $root/work/tools.sh
 
@@ -21,11 +37,11 @@ eval $(
         echo sedoc_url_$section=http://doc.liberty-eiffel.org/api/$section
         echo sedoc_rem_$section=$remote
     done <<EOF
+liberty     liberty:tools
 libraries   liberty:lib
 smarteiffel tools
-wrappers    liberty:wrappers
-liberty     liberty:tools
 tutorial    tutorial
+wrappers    liberty:wrappers
 EOF
 )
 
@@ -46,7 +62,7 @@ done | while read i section args; do
     s=$DOC_ROOT/api/$section
     test -d $s || mkdir -p $s
     cd $s
-    run se doc -verbose -title "Section:\\\\ $section" -short_title "$section" \
+    if run se doc -verbose -title "Section:\\\\ $section" -short_title "$section" \
         -wiki_prefix "http://wiki.liberty-eiffel.org/" \
         -menu_separator '"\\|\\ "' -ariadne_separator '"\\>\\ "' \
         -menu "http://www.gnu.org/software/liberty-eiffel/" '"GNU"' \
@@ -57,7 +73,14 @@ done | while read i section args; do
         -menu "http://apt.liberty-eiffel.org" '"Debian\\ packages"' \
         -menu "http://doc.liberty-eiffel.org" '"Documentation"' \
         -js "$root/resources/eiffeldoc/eiffeldoc.js" -css "$root/resources/eiffeldoc/eiffeldoc.css" \
-        -prune test -prune Local $args
+        -prune test -prune Local $args; then
+
+        if [ $zip == TRUE ]; then
+            cd $DOC_ROOT/api
+            tar cfz ${section}.tgz $section
+            rm -rf $section
+        fi
+    fi
 done
 
 status=$(grep '^se failed with status' $LOG | awk 'BEGIN {i = 0} {if (NF > 0) {i += $NF} else {i++}} END {printf("%d\n", i)}')
