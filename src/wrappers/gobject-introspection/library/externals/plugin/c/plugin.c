26138ea50cbe3b66f9d9ea8f83fa72bf48c88559
tybor <paolo.redaelli@gmail.com>
2013-09-16 00:59:19 +0200

    Refreshing gobject-instrospection wrappers, forgot 2 files

--- src/wrappers/gobject-introspection/library/externals/plugin/c/plugin.c
+++ src/wrappers/gobject-introspection/library/externals/plugin/c/plugin.c
@@ -3,5 +3,5 @@
 ** Any change will be lost by the next execution of the tool.
 */
 #ifndef GOBJECT_INTROSPECTION_LIBERTY_PLUGIN
-#	define GOBJECT_INTROSPECTION_LIBERTY_PLUGIN
+#   define GOBJECT_INTROSPECTION_LIBERTY_PLUGIN
 #endif
