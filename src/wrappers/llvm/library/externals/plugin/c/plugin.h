a58bd583f98387aeabc691038c6d5e8522fc56d4
tybor <paolo.redaelli@gmail.com>
2013-09-16 22:24:38 +0200

    LLVM: forgot plugin.[ch] (as usual 8-/ )

--- src/wrappers/llvm/library/externals/plugin/c/plugin.h
+++ src/wrappers/llvm/library/externals/plugin/c/plugin.h
@@ -2,7 +2,6 @@
 ** This file have been created by wrapper-generator.
 ** Any change will be lost by the next execution of the tool.
 */
-#define sizeof_LLVMOpaqueTypeHandle (sizeof(struct LLVMOpaqueTypeHandle))
 #define sizeof_LLVMOpaquePassManager (sizeof(struct LLVMOpaquePassManager))
 #define sizeof_LLVMOpaqueModuleProvider (sizeof(struct LLVMOpaqueModuleProvider))
 #define sizeof_LLVMOpaqueUse (sizeof(struct LLVMOpaqueUse))
@@ -13,6 +12,7 @@
 #define sizeof_LLVMOpaqueGenericValue (sizeof(struct LLVMOpaqueGenericValue))
 #define sizeof_LLVMOpaqueType (sizeof(struct LLVMOpaqueType))
 #define sizeof_LLVMOpaqueTargetData (sizeof(struct LLVMOpaqueTargetData))
+#define sizeof_LLVMOpaqueTargetLibraryInfotData (sizeof(struct LLVMOpaqueTargetLibraryInfotData))
 #define sizeof_LLVMOpaqueValue (sizeof(struct LLVMOpaqueValue))
 #define sizeof_LLVMOpaqueContext (sizeof(struct LLVMOpaqueContext))
 #define sizeof_LLVMOpaqueModule (sizeof(struct LLVMOpaqueModule))
@@ -20,6 +20,7 @@
 #define sizeof_LLVMOpaquePassRegistry (sizeof(struct LLVMOpaquePassRegistry))
 #define sizeof_TargetData (sizeof(struct TargetData))
 #define sizeof_ExecutionEngine (sizeof(struct ExecutionEngine))
+#define sizeof_TargetLibraryInfo (sizeof(struct TargetLibraryInfo))
 #define sizeof_PassManagerBase (sizeof(struct PassManagerBase))
 #define sizeof_MemoryBuffer (sizeof(struct MemoryBuffer))
 #define sizeof_GenericValue (sizeof(struct GenericValue))
