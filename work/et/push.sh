13b11a1fe8d21846d164967778b081bd41add1ce
Cyril Adrian <cyril.adrian@gmail.com>
2013-10-08 10:57:45 +0200

    OK, ET should sync all known branches to github

--- work/et/push.sh
+++ work/et/push.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 cd /home/et/Liberty_savannah
-branch=`cat ~/branch`
+
 {
-    git checkout $branch
+    git fetch origin
     echo
-    git pull origin
-} > ../pull.out
-git push github $branch 2> push.err > ../push.out
+    for branch in $(git branch | cut -c3-); do
+        git checkout $branch
+        echo
+        git merge --ff-only origin/$branch
+    done
+} 2> pull.err > ../pull.out
+
+git push github : 2> push.err > ../push.out
