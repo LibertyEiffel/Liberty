a6b662d2cf9c1bdfb17547cf9629c37a68688ced
Cyril Adrian <cyril.adrian@gmail.com>
2013-11-04 10:02:59 +0100

    fix ET when switching to a new branch

--- work/et/push.sh
+++ work/et/push.sh
@@ -4,11 +4,10 @@ cd /home/et/Liberty_savannah
 {
     git fetch origin
     echo
-    for branch in $(git branch | cut -c3-); do
+    for branch in $(git branch -r | cut -c3- | grep ^origin/ | grep -v HEAD | cut -c8-); do
         git checkout $branch
         echo
         git merge --ff-only origin/$branch
+        git push github $branch:$branch
     done
 } 2> pull.err > ../pull.out
-
-git push github : 2> push.err > ../push.out
