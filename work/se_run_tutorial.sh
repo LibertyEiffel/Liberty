5c8cdb2462df65256f9bbc7bb1bd2389be05417c
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 20:12:32 +0100

    signal & redirection festival

--- work/se_run_tutorial.sh
+++ work/se_run_tutorial.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
 
-workdir=$(dirname $(readlink -f $0))
-find $(dirname $workdir)/tutorial -name 'aux*' -prune -o -name '*.e' | while read e; do
-    $workdir/se_run.sh "$e"
-done
+export _MY_PID=$$
 
-exit $?
+function failed {
+    exit 1
+}
+
+function success {
+    exit 0
+}
+
+trap failed USR1
+trap success USR2
+
+(
+    workdir=$(dirname $(readlink -f $0))
+    find $(dirname $workdir)/tutorial -name 'aux*' -prune -o -name '*.e' -print | while read e; do
+        $workdir/se_run.sh "$e" || {
+            echo FAILED >&3
+        }
+    done 3> >(
+        grep -q FAILED && {
+            kill -USR1 $_MY_PID 2>/dev/null
+            wait $_MY_PID 2>/dev/null
+        }
+    )
+    kill -USR2 $_MY_PID 2>/dev/null
+    wait $_MY_PID 2>/dev/null
+)
+
+sleep 30
+exit 2
