fc6fdc5529ca8a2303516a7da91eaadf80ce5b78
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 22:43:03 +0100

    KISS

--- work/se_run_tutorial.sh
+++ work/se_run_tutorial.sh
@@ -1,33 +1,12 @@
 #!/usr/bin/env bash
 
-export _MY_PID=$$
+status=0
 
-function failed {
-    exit 1
-}
+file=$(mktemp)
+workdir=$(dirname $(readlink -f $0))
+find $(dirname $workdir)/tutorial -name 'aux*' -prune -o -name '*.e' -print >$file
+while read e; do
+    $workdir/se_run.sh "$e" || status=1
+done <$file
 
-function success {
-    exit 0
-}
-
-trap failed USR1
-trap success USR2
-
-(
-    workdir=$(dirname $(readlink -f $0))
-    find $(dirname $workdir)/tutorial -name 'aux*' -prune -o -name '*.e' -print | while read e; do
-        $workdir/se_run.sh "$e" || {
-            echo FAILED >&3
-        }
-    done 3> >(
-        grep -q FAILED && {
-            kill -USR1 $_MY_PID 2>/dev/null
-            wait $_MY_PID 2>/dev/null
-        }
-    )
-    kill -USR2 $_MY_PID 2>/dev/null
-    wait $_MY_PID 2>/dev/null
-)
-
-sleep 30
-exit 2
+exit $status
