43ddaf0dab906ce03887000acf175719638bafa9
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 11:09:42 +0100

    adding timeout security

--- work/se_run.sh
+++ work/se_run.sh
@@ -3,9 +3,50 @@ cd $(dirname $1)
 e=$(basename $1)
 exe=${e%.e}.exe
 in=${e%.e}.in
+script=${e%.e}.sh
+
+echo
 echo '**** TUTORIAL:' $1
 se c -clean -style_warning -o $exe $e || exit 1
+
+ulimit -t 60
+
 if [ -r $in ]; then
     exec <$in
 fi
-exec ./$exe
+
+pidfile=$(mktemp)
+(
+    ./$exe &
+    pid=$!
+    echo $pid > $pidfile
+    wait
+    ret=$!
+    rm -f $pidfile
+    exit $ret
+) &
+
+exe_pid=$(<$pidfile)
+if [ $? -ne 0 ]; then
+    wait $exe_pid
+    exit $!
+fi
+
+if [ -r $script ]; then
+    ./$script $exe_pid &
+fi
+
+(
+    sleep 30
+    echo "**** Process takes too long, killing!"
+    test -r $pidfile && kill -9 $(<$pidfile)
+)&
+kill_pid=$!
+
+wait $exe_pid
+status=$?
+
+kill $kill_pid
+rm -f $pidfile
+
+exit $status
