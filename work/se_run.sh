f286166926b7c6b173ff72d58ff5539c355a940f
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 06:49:49 +0100

    need info

--- work/se_run.sh
+++ work/se_run.sh
@@ -2,6 +2,6 @@
 cd $(dirname $1)
 e=$(basename $1)
 exe=${e%.e}.exe
-which se
-se c -clean -style_warning -o $exe $e || exit 1
+echo '**** TUTORIAL:' $1
+se c -verbose -clean -style_warning -o $exe $e || exit 1
 exec ./$e
