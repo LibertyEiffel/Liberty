53fb1ea1e09daf9a747e94c8fc8b5093740d13cc
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 10:12:26 +0100

    allow specific tutorial input

--- work/se_run.sh
+++ work/se_run.sh
@@ -2,6 +2,10 @@
 cd $(dirname $1)
 e=$(basename $1)
 exe=${e%.e}.exe
+in=${e%.e}.in
 echo '**** TUTORIAL:' $1
 se c -clean -style_warning -o $exe $e || exit 1
+if [ -r $in ]; then
+    exec <$in
+fi
 exec ./$exe
