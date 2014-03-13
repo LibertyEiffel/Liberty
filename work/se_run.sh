fa496af1f56a0c57c9d5e3e9563bf1d1f3b7a488
Cyril Adrian <cyril.adrian@gmail.com>
2014-03-13 09:48:20 +0100

    verbose not needed anymore, fix exe launch

--- work/se_run.sh
+++ work/se_run.sh
@@ -3,5 +3,5 @@ cd $(dirname $1)
 e=$(basename $1)
 exe=${e%.e}.exe
 echo '**** TUTORIAL:' $1
-se c -verbose -clean -style_warning -o $exe $e || exit 1
-exec ./$e
+se c -clean -style_warning -o $exe $e || exit 1
+exec ./$exe
