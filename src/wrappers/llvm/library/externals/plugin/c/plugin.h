fbe904a3717ce0ca319c5f2e0f3d6ae52107fa16
Paolo Redaelli <paolo.redaelli@gmail.com>
2015-06-20 08:06:22 +0200

    Cleanup in progress: forgotten files of partially implemented wrappers

---
+++ src/wrappers/llvm/library/externals/plugin/c/plugin.h
@@ -0,0 +1,26 @@
+/*
+** This file have been created by wrapper-generator.
+** Any change will be lost by the next execution of the tool.
+*/
+#define sizeof_LLVMOpaquePassManager (sizeof(struct LLVMOpaquePassManager))
+#define sizeof_LLVMOpaqueModuleProvider (sizeof(struct LLVMOpaqueModuleProvider))
+#define sizeof_LLVMOpaqueUse (sizeof(struct LLVMOpaqueUse))
+#define sizeof_LLVMOpaqueBasicBlock (sizeof(struct LLVMOpaqueBasicBlock))
+#define sizeof_LLVMOpaqueExecutionEngine (sizeof(struct LLVMOpaqueExecutionEngine))
+#define sizeof_LLVMStructLayout (sizeof(struct LLVMStructLayout))
+#define sizeof_LLVMOpaqueMemoryBuffer (sizeof(struct LLVMOpaqueMemoryBuffer))
+#define sizeof_LLVMOpaqueGenericValue (sizeof(struct LLVMOpaqueGenericValue))
+#define sizeof_LLVMOpaqueType (sizeof(struct LLVMOpaqueType))
+#define sizeof_LLVMOpaqueTargetData (sizeof(struct LLVMOpaqueTargetData))
+#define sizeof_LLVMOpaqueTargetLibraryInfotData (sizeof(struct LLVMOpaqueTargetLibraryInfotData))
+#define sizeof_LLVMOpaqueValue (sizeof(struct LLVMOpaqueValue))
+#define sizeof_LLVMOpaqueContext (sizeof(struct LLVMOpaqueContext))
+#define sizeof_LLVMOpaqueModule (sizeof(struct LLVMOpaqueModule))
+#define sizeof_LLVMOpaqueBuilder (sizeof(struct LLVMOpaqueBuilder))
+#define sizeof_LLVMOpaquePassRegistry (sizeof(struct LLVMOpaquePassRegistry))
+#define sizeof_TargetData (sizeof(struct TargetData))
+#define sizeof_ExecutionEngine (sizeof(struct ExecutionEngine))
+#define sizeof_TargetLibraryInfo (sizeof(struct TargetLibraryInfo))
+#define sizeof_PassManagerBase (sizeof(struct PassManagerBase))
+#define sizeof_MemoryBuffer (sizeof(struct MemoryBuffer))
+#define sizeof_GenericValue (sizeof(struct GenericValue))
---
+++ src/wrappers/llvm/library/llvm_type_handle.e
@@ -0,0 +1,49 @@
+class LLVM_TYPE_HANDLE
+	-- When building recursive types using (TODO) LLVMRefineType, LLVM_TYPE
+	-- values may become invalid; LLVM_TYPE_HANDLE may be used to resolve this
+	-- problem. See the llvm::AbstractTypeHolder class in the documentation of
+	-- the C++ òobrary.
+ 
+inherit 
+	OPAQUE_TYPE
+	EIFFEL_OWNED 
+
+insert LLVM_TYPE_FACTORY
+
+create {ANY} from_type, from_external_pointer
+
+feature {ANY} 
+	from_type (a_type: LLVM_TYPE) is
+		-- `a_type' is potentially an abstract type.
+	require a_type/=Void
+	do
+		handle:=llvmcreate_type_handle(a_type.handle)
+	end
+
+	type: LLVM_TYPE is
+		-- Current resolved into an effective type. 
+	do
+		Result:=type_wrapper(llvmresolve_type_handle(handle))
+	end
+
+	-- TODO: provide refining support:  void LLVMRefineType(LLVMTypeRef AbstractTy, LLVMTypeRef ConcreteTy);
+
+end -- class LLVM_TYPE_HANDLE
+
+-- Copyright 2009 Paolo Redaelli
+
+-- This file is part of LLVM wrappers for Liberty Eiffel.
+--
+-- This library is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, version 3 of the License.
+--
+-- Liberty Eiffel is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Liberty Eiffel.  If not, see <http://www.gnu.org/licenses/>.
+--
+
