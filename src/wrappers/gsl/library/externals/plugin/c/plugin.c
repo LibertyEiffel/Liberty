b38737e57b7018023e50ad2546f2a96fbe10b0fc
Paolo Redaelli <paolo.redaelli@gmail.com>
2016-05-07 08:15:56 +0200

    added leggow_options.e

--- src/wrappers/gsl/library/externals/plugin/c/plugin.c
+++
@@ -1,7 +0,0 @@
-/*
-** This file have been created by wrapper-generator.
-** Any change will be lost by the next execution of the tool.
-*/
-#ifndef GSL_LIBERTY_PLUGIN
-#	define GSL_LIBERTY_PLUGIN
-#endif
