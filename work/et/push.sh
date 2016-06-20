d3286922a137e8473505bdf92309839b7bbd041f
Raphael Mack <ramack@raphael-mack.de>
2016-06-20 22:04:25 +0200

    we want to overwrite ET-local changes...

--- work/et/push.sh
+++ work/et/push.sh
@@ -6,7 +6,7 @@ cd $HOME/Liberty_savannah
     git fetch origin
     echo
     for branch in $(git branch -r | cut -c3- | grep ^origin/ | grep -v HEAD | cut -c8-); do
-        git checkout $branch
+        git checkout -f $branch
         echo
         git merge --ff-only origin/$branch
     done
